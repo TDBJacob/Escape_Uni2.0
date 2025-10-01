e280ac938479e151ea4c41fdf21f8131a31586b3
Charlotte <128639326+CharlotteScott1@users.noreply.github.com>
2025-10-01 10:58:31 +0100

    Feat: add code
    Initial project skeleton upload

---
+++ core/src/main/java/io/github/team6ENG/EscapeUni/GameScreen.java
@@ -0,0 +1,51 @@
+package io.github.team6ENG.EscapeUni;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen implements Screen {
+
+
+    private final Main game;
+
+    public GameScreen(final Main game) {
+        this.game = game;
+    }
+
+        @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        ScreenUtils.clear(Color.RED);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
