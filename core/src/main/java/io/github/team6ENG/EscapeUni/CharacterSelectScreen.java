f687aeba99830a7d225a7fc6ecd35d5275ef0e1b
CharlotteScott <128639326+CharlotteScott1@users.noreply.github.com>
2025-10-03 21:04:06 +0100

    feat:Added buttons to CharacterSelectScreen

--- core/src/main/java/io/github/team6ENG/EscapeUni/CharacterSelectScreen.java
+++ core/src/main/java/io/github/team6ENG/EscapeUni/CharacterSelectScreen.java
@@ -3,15 +3,56 @@ package io.github.team6ENG.EscapeUni;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.util.Stack;
 
 public class CharacterSelectScreen implements Screen {
 
 
     private final Main game;
+    private Stage stage;
+    private Table table;
 
     public CharacterSelectScreen(final Main game) {
         this.game = game;
+
+        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight()));
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        //placeholder buttons, maybe change to image button when we have sprites
+        final TextButton characterButton1 = new TextButton("Character 1", game.buttonSkin);
+        table.add(characterButton1).expand().bottom().width(300).height(100).pad(20);
+        TextButton characterButton2 = new TextButton("Character 2", game.buttonSkin);
+        table.add(characterButton2).expand().bottom().width(300).height(100).pad(20);
+
+
+        characterButton1.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //TODO: Set sprite here
+                game.setScreen(new GameScreen(game));
+                dispose();
+            } });
+        characterButton2.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //TODO: Set sprite here
+                game.setScreen(new GameScreen(game));
+                dispose();
+            }
+        });
     }
 
     @Override
@@ -31,15 +72,11 @@ public class CharacterSelectScreen implements Screen {
         game.menuFont.draw(game.batch, "Character selection screen", game.viewport.getScreenWidth()/200 - 2, game.viewport.getScreenHeight()/200 +1.5f );
         game.menuFont.draw(game.batch, "Display 2 characters to choose from", game.viewport.getScreenWidth()/200 - 3, game.viewport.getScreenHeight()/200 + 0.5f);
 
-
         game.batch.end();
 
+        stage.act();
+        stage.draw();
 
-        if (Gdx.input.justTouched()) {
-
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }
 
     }
 
