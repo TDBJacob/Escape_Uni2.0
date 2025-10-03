4821330fa4784d416f726737ae5b386866fabdca
CharlotteScott <128639326+CharlotteScott1@users.noreply.github.com>
2025-10-03 08:50:04 +0100

    feat: Added base character select screen
    Created new screen for character selection.

--- core/src/main/java/io/github/team6ENG/EscapeUni/CharacterSelectScreen.java
+++ core/src/main/java/io/github/team6ENG/EscapeUni/CharacterSelectScreen.java
@@ -5,62 +5,66 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MainMenuScreen implements Screen {
+public class CharacterSelectScreen implements Screen {
 
-    final Main game;
 
-    public MainMenuScreen(final Main game) {
+    private final Main game;
 
+    public CharacterSelectScreen(final Main game) {
         this.game = game;
     }
 
     @Override
-    public void render(float delta) {
-        ScreenUtils.clear(Color.BLACK);
+    public void show() {
 
+    }
+
+    @Override
+    public void render(float delta) {
+
+        ScreenUtils.clear(Color.GREEN);
         game.viewport.apply();
         game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
 
         game.batch.begin();
 
-        game.menuFont.draw(game.batch, "Escape York University", game.viewport.getScreenWidth()/200 - 2, game.viewport.getScreenHeight()/200 +1.5f );
-        game.menuFont.draw(game.batch, "Click anywhere to play", game.viewport.getScreenWidth()/200 - 2, game.viewport.getScreenHeight()/200 + 0.5f);
+        game.menuFont.draw(game.batch, "Character selection screen", game.viewport.getScreenWidth()/200 - 2, game.viewport.getScreenHeight()/200 +1.5f );
+        game.menuFont.draw(game.batch, "Display 2 characters to choose from", game.viewport.getScreenWidth()/200 - 3, game.viewport.getScreenHeight()/200 + 0.5f);
 
 
         game.batch.end();
 
+
         if (Gdx.input.justTouched()) {
 
-            game.setScreen(new CharacterSelectScreen(game));
+            game.setScreen(new GameScreen(game));
             dispose();
         }
+
     }
+
     @Override
     public void resize(int width, int height) {
-        game.viewport.update(width, height, true);
-    }
 
-
-    @Override
-    public void show() {
-    }
-
-    @Override
-    public void hide() {
     }
 
     @Override
     public void pause() {
+
     }
 
     @Override
     public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
     }
 
     @Override
     public void dispose() {
 
     }
-
 }
-
--- core/src/main/java/io/github/team6ENG/EscapeUni/GameScreen.java
+++ core/src/main/java/io/github/team6ENG/EscapeUni/GameScreen.java
@@ -1,5 +1,6 @@
 package io.github.team6ENG.EscapeUni;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -22,6 +23,24 @@ public class GameScreen implements Screen {
     public void render(float delta) {
 
         ScreenUtils.clear(Color.RED);
+
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+
+        game.batch.begin();
+
+        game.menuFont.draw(game.batch, "Main menu screen", game.viewport.getScreenWidth()/200 - 2, game.viewport.getScreenHeight()/200 +1.5f );
+        game.menuFont.draw(game.batch, "Add game :)", game.viewport.getScreenWidth()/200 - 2, game.viewport.getScreenHeight()/200 + 0.5f);
+
+
+        game.batch.end();
+
+        // Cycle through screens for testing, remove later
+        if (Gdx.input.justTouched()) {
+
+            game.setScreen(new MainMenuScreen(game));
+            dispose();
+        }
     }
 
     @Override
