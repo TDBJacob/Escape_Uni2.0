e76a7a5f9c2d5b8c1b0bff7b5c613eb5629411a8
Zhuoran Fu <1252738357@qq.com>
2025-10-06 04:14:46 +0800

    feat: add player movement logic and implementation

--- core/src/main/java/io/github/team6ENG/EscapeUni/Main.java
+++ core/src/main/java/io/github/team6ENG/EscapeUni/Main.java
@@ -1,12 +1,11 @@
 package io.github.team6ENG.EscapeUni;
 
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class Main extends Game{
@@ -14,20 +13,28 @@ public class Main extends Game{
     public SpriteBatch batch;
     public BitmapFont menuFont;
     public FitViewport viewport;
-
+    public Skin buttonSkin;
 
     public void create() {
         batch = new SpriteBatch();
         // use libGDX's default font
         menuFont = new BitmapFont(Gdx.files.internal("fonts/menuScreenFont.fnt"));
-        viewport = new FitViewport(8, 5);
+        viewport = new FitViewport(800, 600);
 
         //Scale font to our viewport by ratio of viewport height to screen height
         menuFont.setUseIntegerPositions(false);
-        menuFont.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
+        menuFont.getData().setScale(0.8f);
         menuFont.setColor(Color.valueOf("4287f5FF"));
 
+        buttonSkin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+
         this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        super.resize(width, height);
     }
 
     public void render() {
@@ -35,8 +42,15 @@ public class Main extends Game{
     }
 
     public void dispose() {
-        batch.dispose();
-        menuFont.dispose();
+       // dispose resources in reverse creation order
+        if (buttonSkin != null) {
+            buttonSkin.dispose();
+        }
+        if (menuFont != null) {
+            menuFont.dispose();
+        }
+        if (batch != null) {
+            batch.dispose();
+        }
     }
-
 }
