ef000561ea1ee066f6f44299e0a1343c677d0a86
Zhuoran Fu <1252738357@qq.com>
2025-10-10 23:24:24 +0800

    feat: add lighting effects

--- core/src/main/java/io/github/team6ENG/EscapeUni/SpriteAnimations.java
+++ core/src/main/java/io/github/team6ENG/EscapeUni/SpriteAnimations.java
@@ -1,28 +1,20 @@
 package io.github.team6ENG.EscapeUni;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-
-import java.util.Dictionary;
-import java.util.Enumeration;
 import java.util.HashMap;
 
 public class SpriteAnimations  {
 
-    public HashMap<String, Animation<TextureRegion>> animations = new HashMap<String, Animation<TextureRegion>>();
+    public HashMap<String, Animation<TextureRegion>> animations = new HashMap<>();
     public float x;
     public float y;
-    private HashMap<String, Integer[]> animationInfo;
+    private HashMap<String, Integer[]> animationInfo = new HashMap<>();
     private int COLUMNS;
     private int ROWS;
     private Texture sheet;
-
-
-
 
     // Used to generate animations dictionary which contains all animations for that sprite
     public SpriteAnimations(String file, int SheetCols, int SheetRows) {
@@ -47,9 +39,16 @@ public class SpriteAnimations  {
                 frames[i] = tmp[animationInfo.get(key)[0]][i];
                 System.out.println(frames[i]);
             }
-            animations.put(key, new Animation<TextureRegion>(0.5f, frames));
+            animations.put(key, new Animation<>(0.5f, frames));
 
         }
     }
+
+     public void dispose() {
+        if (sheet != null) {
+            sheet.dispose();
+        }
+    }
+
 
 }
