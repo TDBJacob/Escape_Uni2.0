305579d99f8d208b3314dad1aa452633100b34fe
mzq513 <mzq513@york.ac.uk>
2025-12-05 17:01:45 +0000

    started making enemy class

---
+++ core/src/main/java/io/github/team9/escapefromuni/Coordinate.java
@@ -0,0 +1,24 @@
+package io.github.team9.escapefromuni;
+
+public class Coordinate {
+    public int x;
+    public int y;
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}
