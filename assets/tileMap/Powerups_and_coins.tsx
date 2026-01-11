93d8ddfdc745bc9d2c665de8c78a4732f4f86738
vpb513-png <vpb513@york.ac.uk>
2026-01-11 19:40:04 +0000

    Added powerups + tests + stationary enemies

---
+++ assets/tileMap/Powerups_and_coins.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Powerups_and_coins" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../items/Custom_coin_sprite.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../powerups/Neptune_planet_v2.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../powerups/Uranus_planet.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../items/Fiver sprite.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../items/Essay.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../enemies/Dean.png" width="32" height="32"/>
+ </tile>
+</tileset>
